00fc9bbdfb81b1015ccf69103f1909a6e6dcf640
Xanemili <emili.xander@gmail.com>
2020-12-29 11:50:15 -0900

    reworking to bring in iex

--- src/components/Dashboard/StockPrice.jsx
+++ src/components/Dashboard/StockPrice.jsx
@@ -1,15 +1,19 @@
-import React, {useState, useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 import { ListItemText } from '@material-ui/core'
 
 
-const StockPrice = ({data}) => {
+const StockPrice = ({price}) => {
+
+  // temporary stop gap to show stock price data. will be swapped to a socket for entire portfolio.
 
   return (
     <>
-    {data ?
+    {console.log(price)}
+    {price ?
       <ListItemText
-      primary={data.c ? `$ ${(data.c).toFixed(2)}` : 0}
-      secondary={data.c ? `${(((data.c / data.o) - 1) * 100).toFixed(2)}%` : 0}
+      primary={price.lastSalePrice ? `$ ${(price.lastSalePrice).toFixed(2)}` : 0}
+      // will take extra data calls at this point to show %, will add.
+      // secondary={price ? `${(((price / price) - 1) * 100).toFixed(2)}%` : 0}
       >
       </ListItemText> : <></>
     }
